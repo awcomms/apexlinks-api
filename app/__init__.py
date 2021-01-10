83c41d728359753a4f27ce399420f751c35ed68b
Edward Gold Edem Hogan <edge3769@gmail.com>
2021-01-10 17:46:12 +0100

    changed to `MAIL_EMAIL`, added gunicorn to requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -31,8 +31,8 @@ def create_app():
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
             auth = None
-            if app.config['MAIL_email'] or app.config['MAIL_PASSWORD']:
-                auth = (app.config['MAIL_email'], app.config['MAIL_PASSWORD'])
+            if app.config['MAIL_EMAIL'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_EMAIL'], app.config['MAIL_PASSWORD'])
             secure = None
             if app.config['MAIL_USE_TLS']:
                 secure = ()
