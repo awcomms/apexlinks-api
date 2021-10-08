8099400a8ad74ad44a9541e3e3d8381fcd9ec043
EdwardGold Edem Hogan <edge37@outlook.com>
2021-10-08 04:33:49 +0100

    mmhmm

--- app/auth.py
+++ app/auth.py
@@ -16,6 +16,7 @@ def cred(f):
             password = s[1]
             return f(*args, **kwargs, username=username, password=password)
         except Exception as e:
+            print('@cred error', e)
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
