96a8f1250c77d8a8d41a02980fcd60c80b148da8
EdwardGold Edem Hogan <edge37@outlook.com>
2021-10-08 10:03:29 +0100

    corrected error on /pay

--- app/api/pay.py
+++ app/api/pay.py
@@ -6,17 +6,17 @@ from datetime import timezone
 from datetime import timedelta
 from flask import request
 from flask import current_app
-from pypaystack import Transaction, transactions
+from pypaystack import Transaction
 from app.user_model import User
 from app.api import bp
 from app import db
 
 @bp.route('/pay/check', methods=['GET'])
 def check():
-    test = current_app.config.PAYSTACK_TEST
-    test_key = current_app.config.PAYSTACK_TEST_KEY
-    live_key = current_app.config.PAYSTACK_LIVE_KEY
-    key = test_key if test else live_key
+    test = current_app.config['PAYSTACK_TEST']
+    test_key = current_app.config['PAYSTACK_TEST_KEY']
+    live_key = current_app.config['PAYSTACK_LIVE_KEY']
+    key = test_key if test == 'True' else live_key
     transaction = Transaction(token_key=key)
     duration = timedelta(days=30)
     for user in User.query:
@@ -32,11 +32,13 @@ def check():
 @bp.route('/pay', methods=['POST'])
 def pay():
     print('/pay')
-    test = current_app.config.PAYSTACK_TEST
-    test_key = current_app.config.PAYSTACK_TEST_KEY
-    live_key = current_app.config.PAYSTACK_LIVE_KEY
-    key = test_key if test else live_key
+    test = current_app.config['PAYSTACK_TEST']
+    test_key = current_app.config['PAYSTACK_TEST_KEY']
+    live_key = current_app.config['PAYSTACK_LIVE_KEY']
+    key = test_key if test == 'True' else live_key
     data = request.get_json()
+    if not request.data:
+        return '', 400
     hash = hmac.new(key, request.data, digestmod=hashlib.sha512).hexdigest()
     request_hash = request.headers.get('X-Paystack-Signature')
     if not hmac.compare_digest(hash, request_hash):
@@ -52,10 +54,10 @@ def pay():
 
 @bp.route('/pay/is_paid', methods=['GET'])
 def is_paid():
-    test = current_app.config.PAYSTACK_TEST
-    test_key = current_app.config.PAYSTACK_TEST_KEY
-    live_key = current_app.config.PAYSTACK_LIVE_KEY
-    key = test_key if test else live_key
+    test = current_app.config['PAYSTACK_TEST']
+    test_key = current_app.config['PAYSTACK_TEST_KEY']
+    live_key = current_app.config['PAYSTACK_LIVE_KEY']
+    key = test_key if test == 'True' else live_key
     ref = request.args.get('ref')
     transaction = Transaction(token_key=key)
     res = transaction.verify(ref)
